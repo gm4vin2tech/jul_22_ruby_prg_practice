1cfce8dde7bb81d38941c916eb3848d11f501460
Gowthami Palla <Gowthami.P@sparinfosys.com>
2022-07-28 08:51:45 +0530

    first_prg

---
+++ first_prg.rb
@@ -0,0 +1,11 @@
+#this is first prg
+# this is single line comments
+
+=begin
+	This is regarding documentation
+	this is multi line comments
+=end
+
+puts "hello world" #\n newline #frequent usage
+print "welcome"  #no \n
+p "Thanks"
