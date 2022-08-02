cc1dced0a47df687ae342123e6f43be8fc7bf19a
Gowthami Palla <Gowthami.P@sparinfosys.com>
2022-08-02 08:30:14 +0530

    loops_demo.rb

---
+++ loops_demo.rb
@@ -0,0 +1,126 @@
+# loops - while, do-while, for, until
+
+# puts "welcome !!!!"
+
+#while loop
+# i = 0
+# while i < 5
+# 	puts "this is from while conition i: #{i}"
+# 	i += 1 #i = i+1
+# end	
+
+# puts "external from loop"
+
+#until loop
+# i = 15
+# until i < 5
+# 	puts "this is from until conition i: #{i}"
+# 	i -= 1 #i = i-1
+# end	
+
+# puts "external from loop"
+
+# do-while loop
+# i = 0
+# loop do
+# 	break if i == 5
+# 	puts "this is do-while i: #{i}"
+# 	i+=1
+# end
+
+#infinite and finite - break
+
+#for loop - break and next (skips the current execution)
+# for i in 0..5
+# 	break if i == 3
+# 	puts i
+# end
+
+# Iterators
+arr = [1,4,8,34,56]
+hsh = {"a" => 123, "b" => 345}
+
+# arr.each do |ele|
+# 	puts ele
+# 	puts ele
+# end
+
+# arr.each { |e| puts e }
+
+# puts arr
+
+# arr.map { |e| puts e }
+
+# puts arr
+
+# arr.collect { |e| puts e }
+
+# puts arr
+
+# str = "the line
+# the seconf line
+# this is"
+
+# str.each_line do |line|
+# 	puts line
+# end
+
+# times iterator
+
+# 7.times do |t|
+# 	puts 3+4
+# 	puts 4-8
+# 	puts "somethinf"
+# end
+
+
+# puts "somethinf" * 7 #repetion operator
+
+# up_to and down_to iterator
+
+# 4.upto(7) do |n|
+#   puts n
+# end
+
+# 14.downto(7) do |n|
+#   puts n
+# end
+
+# step iterator
+# (0..100).step(20) do|i|
+#    puts i
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
